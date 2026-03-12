e1bdd7d103942245595bbfb4b86bd44118983951
YatoNorai <edilsonmauro1727@gmail.com>
2026-03-12 00:47:07 -0400

    Revert "chore: init packages branch"
    This reverts commit d676218aa7599fa3512ea6d2158fec4ccbb7d7c8.

---
+++ generate-apt-repo.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+#
+# Copyright (C) 2025 Akash Yadav
+#
+# This file is part of The Scribe Project.
+#
+# Scribe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scribe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Scribe.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+set -euo pipefail
+
+script=$(realpath "$0")
+script_dir=$(dirname "$script")
+
+# shellcheck source=common.sh
+. "$script_dir/common.sh"
+
+# Supported architectures
+declare -a ARCHS=(
+    "aarch64"
+    "arm"
+)
+
+termux_packages="$script_dir/termux-packages"
+output_dir="$script_dir/output"
+repo_dir="${COTG_REPO_DIR}"
+debs_dir="$output_dir/debs"
+termux_apt_repo="$output_dir/termux-apt-repo"
+
+# Clean
+rm -rf "$debs_dir"
+rm -rf "$repo_dir"
+
+if ! [ -f "$termux_apt_repo" ]; then
+    # Download termux-apt-repo script
+    wget https://github.com/termux/termux-apt-repo/raw/refs/heads/master/termux-apt-repo -O "$termux_apt_repo"
+
+    # Make it executable
+    chmod +x "$termux_apt_repo"
+fi
+
+# Create dirs
+mkdir -p "$debs_dir"
+mkdir -p "$repo_dir"
+
+# Add symlinks to deb files to the repo dir
+for arch in "${ARCHS[@]}"; do
+    find "$output_dir/$arch"\
+        -mindepth 1\
+        -maxdepth 1\
+        -type f\
+        -name "*.deb"\
+        -exec ln -sf {} "$debs_dir/" \; ||\
+        scribe_error_exit "Failed to symlink '$arch' debs"
+done
+
+# Generate APT repository
+"$termux_apt_repo" "$debs_dir" "$repo_dir" stable main ||\
+    scribe_error_exit "Failed to create local API repository"
+
+# Clean
+rm -rf "$debs_dir"
---
+++ packages.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+# Base packages, common for both debug and release builds
+declare -a COTG_PACKAGES__BASE
+
+# These are the variant-specific additional packages
+# that are included in bootstrap archives
+declare -a COTG_PACKAGES__DEBUG
+declare -a COTG_PACKAGES__RELEASE
+
+# List of all packages
+# This is used to list all the packages
+# that we need to build
+declare -a COTG_PACKAGES
+
+COTG_PACKAGES__BASE=(
+
+    ## ---- Bootstrap packages ---- ##
+
+    # Core utilities.
+    "apt"
+    "bash"
+    "coreutils"
+    "dash"
+    "diffutils"
+    "findutils"
+    "gawk"
+    "grep"
+    "gzip"
+    "less"
+    "libbz2"
+    "procps"
+    "psmisc"
+    "sed"
+    "tar"
+    "termux-core"
+    "termux-exec"
+    "termux-keyring"
+    "termux-tools"
+    "util-linux"
+
+    # Additional.
+    "brotli"
+    "ed"
+    "debianutils"
+    "dos2unix"
+    "git"
+    "inetutils"
+    "lsof"
+    "mandoc"
+    "nano"
+    "net-tools"
+    "openjdk-21"
+    "patch"
+    "unzip"
+    "zip"
+)
+
+# debug-only packages
+COTG_PACKAGES__DEBUG=(
+    "binutils-libs"
+    "coreutils"
+    "file"
+    "libsqlite"
+    "python"
+    "python-pip"
+    "vim"
+    "wget"
+    "which"
+)
+
+# release-only packages
+COTG_PACKAGES__RELEASE=()
+
+# All packages
+COTG_PACKAGES=(
+    "${COTG_PACKAGES__BASE[@]}"
+    "${COTG_PACKAGES__DEBUG[@]}"
+    "${COTG_PACKAGES__RELEASE[@]}"
+)
+
+# Extra packages that need to be available
+# for use in Code On the Go
+# Note: When adding new packages here,
+#   mention the reason for inclusion
+COTG_PACKAGES+=(
+
+    # Required for self-bootstrapping Code On the Go
+    "libprotobuf"
+
+    # Commonly used tools
+    "wget"
+
+    # cmake and libllvm for Android
+    # useful for Android SDK
+    "cmake"
+    "libllvm"
+)
