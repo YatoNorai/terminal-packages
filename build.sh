8718f2de2c44704a946d72618e09327e1c7a1ba1
YatoNorai <154769729+YatoNorai@users.noreply.github.com>
2026-03-12 14:36:24 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,6 +26,9 @@ script_dir=$(dirname "$script")
 . "${script_dir}/common.sh"
 
 declare -a PATCHES=(
+    # Remove command-not-found from bash Recommends (not available in our repo)
+    "bash-remove-recommends.patch"
+    
     # Fix giflib download URL (SourceForge mirror broken)
     "giflib-fix-srcurl.patch"
 
